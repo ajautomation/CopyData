e03e7822cc5194016f87f4116f0b2cfc078702da
AJ <86994382+ajautomation@users.noreply.github.com>
2023-01-15 20:31:52 +0530

    Update DeleteProjects.java

--- src/test/java/tests/DeleteProjects.java
+++ src/test/java/tests/DeleteProjects.java
@@ -3,7 +3,6 @@ package tests;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.Test;
